4bb71db5353750a3dd17ad0ccb95ff01ed544b7f
Helix128 <diegoatari8@gmail.com>
2026-06-14 01:15:35 -0400

    created project factory and options

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,2 +1,3 @@
 export type { DesignElement, LineElement, RectElement, Screen } from "./design";
-export type { DeviceConfig, Project } from "./project";
+export { createProject } from "./project";
+export type { CreateProjectOptions, DeviceConfig, Project } from "./project";
--- src/core/project.ts
+++ src/core/project.ts
@@ -12,3 +12,30 @@ export type DeviceConfig = {
   width: number;
   height: number;
 };
+
+export type CreateProjectOptions = {
+  name?: string;
+  width?: number;
+  height?: number;
+};
+
+export function createProject(options: CreateProjectOptions = {}): Project {
+  const screenId = "screen-1";
+
+  return {
+    schemaVersion: 1,
+    name: options.name ?? "Untitled",
+    device: {
+      width: options.width ?? 128,
+      height: options.height ?? 64,
+    },
+    screens: [
+      {
+        id: screenId,
+        name: "Screen 1",
+        elements: [],
+      },
+    ],
+    activeScreenId: screenId,
+  };
+}
